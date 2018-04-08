30ee7203fe2631cbcac126b63d8523b5e41eceeb
gqfiddler <garrett.fiddler@gmail.com>
2018-04-07 17:49:36 -0700

    finished 1.2.4 query exercises

--- Thinkful/drills/SQLsnippets.sql
+++ Thinkful/drills/SQLsnippets.sql
@@ -11,7 +11,7 @@ ORDER BY duration
 
 -- Every column of the stations table for station id 84
 SELECT
-*
+  *
 FROM
 	stations
 WHERE
@@ -67,3 +67,73 @@ FROM
 	trips
 GROUP BY end_station
 ORDER BY avg_duration
+
+-- 1.2.4
+
+-- 1. What are the three longest trips on rainy days?
+SELECT
+	trips.trip_id,
+	weather.Date,
+	trips.duration
+FROM trips
+JOIN weather
+ON
+	weather.Date == DATE(trips.start_date) AND
+	weather.ZIP == trips.zip_code
+WHERE weather.Events == 'Rain'
+ORDER BY trips.duration DESC
+
+
+-- 2. Which station is full most often?
+-- NOTE: this doesn't work with the abbreviated status dataset - and the full set
+-- is too slow for my machine
+SELECT
+	station_id,
+	docks_available,
+	timestamp,
+	COUNT(*) minutes_full
+FROM status_abbreviated
+WHERE docks_available == 0
+GROUP BY station_id
+ORDER BY minutes_full DESC
+
+-- 3. Return a list of stations with a count of number of trips starting at
+--    that station but ordered by dock count.
+WITH departure_counts
+AS (
+	SELECT
+		start_station,
+		COUNT(*) departures
+	FROM trips
+	GROUP BY start_station
+)
+
+SELECT
+	stations.dockcount,
+	stations.name,
+	departure_counts.departures
+FROM stations
+JOIN departure_counts
+ON stations.name == departure_counts.start_station
+ORDER BY stations.dockcount
+
+
+-- 4.(Challenge) What's the length of the longest trip for each day it rains anywhere?
+
+-- first, must make a table with only one rain status per date
+WITH rainy_days
+AS (
+	SELECT Date
+	FROM weather
+	WHERE Events == 'Rain'
+	GROUP BY Date
+)
+-- then merge with trips table and sort by duration
+SELECT
+	trips.trip_id,
+	rainy_days.Date,
+	trips.duration
+FROM trips
+JOIN rainy_days
+ON rainy_days.Date == DATE(trips.start_date)
+ORDER BY trips.duration DESC
